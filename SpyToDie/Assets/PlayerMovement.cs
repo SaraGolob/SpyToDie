afe7b1de083a6e60a5b8a1b52939cb56b2616f58
Tano332 <jojjo02@live.se>
2022-03-26 21:06:06 +0100

    First version of map barrier collisions
    added colliders with barriers

--- SpyToDie/Assets/PlayerMovement.cs
+++ SpyToDie/Assets/PlayerMovement.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rigidBody;
     public Animator animator;
+    public Tilemap obstacles;
     Vector2 movement;
 
     // Update is called once per frame
@@ -21,10 +23,22 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
-    // Update is called 50 times second
+    // Update is called 50 times per second
     void FixedUpdate()
     {
         // Movement
-        rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        Vector3Int obstacleMapTile = obstacles.WorldToCell(rigidBody.position + (movement/2) - new Vector2(0, 0.5f));
+
+        if (obstacles.GetTile(obstacleMapTile) == null)
+        {
+            rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(rigidBody.position + (movement/2) - new Vector2(0, 0.5f), 0.2f);
     }
 }
