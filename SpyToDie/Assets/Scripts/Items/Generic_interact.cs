85799541af36cc56f8caa026ce47d9cb5e218cc2
SaraGolob <89583211+SaraGolob@users.noreply.github.com>
2022-03-31 11:54:18 +0200

    light and events
    lighting and events

---
+++ SpyToDie/Assets/Scripts/Items/Generic_interact.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Generic_interact : Interactable
+{
+    public UnityEvent simpleEvent;
+    public override void Interact()
+    {
+        simpleEvent?.Invoke();
+    }
+}
