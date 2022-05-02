d77a9d41d8f42308e6d684d6c5cdd55812115e89
Edvper <edvin1.persson@hotmail.se>
2022-05-02 16:07:52 +0200

    Think it works

--- SpyToDie/Assets/Scripts/Multiplayer/SpawnPlayers.cs
+++ SpyToDie/Assets/Scripts/Multiplayer/SpawnPlayers.cs
@@ -7,8 +7,8 @@ using UnityEngine.UI;
 public class SpawnPlayers : MonoBehaviour
 {
     // Start is called before the first frame update
-    //public GameObject playerPrefab;
-    //public static Transform playerTrans;
+    public GameObject playerPrefab;
+    public static Transform playerTrans;
     public Text text;
     //Coordinates on where you can spawn on a specific place
 
@@ -17,6 +17,6 @@ public class SpawnPlayers : MonoBehaviour
         //Vector3 randomPos = new Vector3(0,0,0);
         //PhotonNetwork.Instantiate(playerPrefab.name, randomPos, Quaternion.identity);
         text.text = TransferableVariabels.ID;
-        //playerTrans = playerPrefab.transform;
+        playerTrans = playerPrefab.transform;
     }
 }
