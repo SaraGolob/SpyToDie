6fbf13c235722990a79b9d2e46249a74e98276de
NielsEriksson <89368371+NielsEriksson@users.noreply.github.com>
2022-05-13 13:00:46 +0200

    aa

--- SpyToDie/Assets/Scripts/Utility/Menu/MenuScript.cs
+++ SpyToDie/Assets/Scripts/Utility/Menu/MenuScript.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class MenuScript : MonoBehaviour
 {
     public Slider slider;
     public UnityEvent fullSlider;
+    public TMP_InputField input;
     public void SinglePlayerPlayButton()
     {
         SceneManager.LoadScene("TutorialControls");
@@ -24,6 +27,14 @@ public class MenuScript : MonoBehaviour
     public void BackButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void LoadMultiPlayer()
+    {
+        SceneManager.LoadScene("LoadingMultiplayerScreen");
+    }
+    public void ClearInput()
+    {
+        input.text = "";
     }
 
 }
