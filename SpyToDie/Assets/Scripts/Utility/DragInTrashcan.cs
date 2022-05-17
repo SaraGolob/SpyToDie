0aee3edbd77c63f17c84d20b9602c2429fab3ede
Edvper <edvin1.persson@hotmail.se>
2022-05-17 20:05:16 +0200

    Added a way to move stuff in the trashcan

--- SpyToDie/Assets/Scripts/Utility/DragInTrashcan.cs
+++ SpyToDie/Assets/Scripts/Utility/DragInTrashcan.cs
@@ -3,8 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragInTrashcan : MonoBehaviour, IPointerDownHandler
+public class DragInTrashcan : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+        rectTransform.anchoredPosition += eventData.delta;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     public void OnPointerDown(PointerEventData eventData)
     {
