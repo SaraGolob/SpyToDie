6ca6c9d2f73fb877dc38e41d25df059203abc454
BenDeLaBod <benjamin.bodin02@gmail.com>
2022-05-18 16:23:26 +0200

    fixed trash puzzle
    removed find from trash puzzle, removed unnecessary game object

--- SpyToDie/Assets/Scripts/Puzzles/TrashCanPuzzle/CheckTrashPuzzle.cs
+++ SpyToDie/Assets/Scripts/Puzzles/TrashCanPuzzle/CheckTrashPuzzle.cs
@@ -6,20 +6,14 @@ using UnityEngine.Events;
 public class CheckTrashPuzzle : MonoBehaviour
 {
     public UnityEvent simpleEvent;
-    public bool puzzleObject1, puzzleObject2, puzzleObject3, puzzleObject4, puzzleObject5;
+    public TrashCanPuzzle puzzleObject1, puzzleObject2, puzzleObject3, puzzleObject4, puzzleObject5;
     public static bool isSolved;
 
     void Update()
     {
         if (!isSolved)
         {
-            puzzleObject1 = GameObject.Find("PuzzleObject1").GetComponent<TrashCanPuzzle>().isActive;
-            puzzleObject2 = GameObject.Find("PuzzleObject2").GetComponent<TrashCanPuzzle>().isActive;
-            puzzleObject3 = GameObject.Find("PuzzleObject3").GetComponent<TrashCanPuzzle>().isActive;
-            puzzleObject4 = GameObject.Find("PuzzleObject4").GetComponent<TrashCanPuzzle>().isActive;
-            puzzleObject5 = GameObject.Find("PuzzleObject5").GetComponent<TrashCanPuzzle>().isActive;
-
-            isSolved = puzzleObject1 && puzzleObject2 && puzzleObject3 && puzzleObject4 && puzzleObject5;
+            isSolved = puzzleObject1.isActive && puzzleObject2.isActive && puzzleObject3.isActive && puzzleObject4.isActive && puzzleObject5.isActive;
 
             if (isSolved)
             {
@@ -30,11 +24,7 @@ public class CheckTrashPuzzle : MonoBehaviour
     }
     public void AutoSolve()
     {
-        GameObject.Find("PuzzleObject1").GetComponent<TrashCanPuzzle>().isActive = true;
-        GameObject.Find("PuzzleObject2").GetComponent<TrashCanPuzzle>().isActive = true;
-        GameObject.Find("PuzzleObject3").GetComponent<TrashCanPuzzle>().isActive = true;
-        GameObject.Find("PuzzleObject4").GetComponent<TrashCanPuzzle>().isActive = true;
-        GameObject.Find("PuzzleObject5").GetComponent<TrashCanPuzzle>().isActive = true;
+        puzzleObject1.isActive = puzzleObject2.isActive = puzzleObject3.isActive = puzzleObject4.isActive = puzzleObject5.isActive = true;
         isSolved = true;
     }
 
